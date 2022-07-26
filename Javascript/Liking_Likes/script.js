294e0c1ddf202a344c6be9a3778c21c361c69511
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-07-25 23:53:18 -0700

    initial commit

---
+++ Javascript/Liking_Likes/script.js
@@ -0,0 +1,14 @@
+var like1 = document.querySelector("#post1");
+var like2 = document.querySelector("#post2");
+var like3 = document.querySelector("#post3");
+
+
+function likes1() {
+    like1.innerHTML = "10 like(s)";
+}
+function likes2() {
+    like2.innerHTML = "13 like(s)";
+}
+function likes3() {
+    like3.innerHTML = "10 like(s)";
+}
