fe695836cfecb32b3518e9c33ba456e9f0daec59
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-07-25 22:03:49 -0700

    initial commit

--- Javascript/Dojo_Pizzeria/script.js
+++ Javascript/Dojo_Pizzeria/script.js
@@ -61,11 +61,28 @@ var toppings = [
     "anchovies"
 ];
 
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
 function randomPizza() {
     var pizza = {};
-    pizza.crustType = crustType;
-    pizza.sauceType = sauceType;
-    pizza.cheeses = cheeses;
-    pizza.toppings = toppings;
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
     return pizza;
 }
+
+console.log(randomPizza());
