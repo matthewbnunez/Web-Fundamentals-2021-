9a88c44b6807dae4d14cb62b6ed0e7df78726a8e
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-07-25 22:03:33 -0700

    initial commit

--- Javascript/Dojo_Pizzeria/script.js
+++ Javascript/Dojo_Pizzeria/script.js
@@ -10,11 +10,62 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
 var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
 console.log(p1);
 
-var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "anchovies", "onions"]);
 console.log(p2);
 
-var p3 = pizzaOven("deep dish", "marinara", ["mozzarella", "feta"], ["mushrooms", "onions"]);
+var p3 = pizzaOven("deep dish", "bbq", ["mozzarella", "parmesan"], ["mushrooms"]);
 console.log(p3);
 
-var p4 = pizzaOven("hand tossed", "traditional", ["mozzarella", "chedder"], ["mushrooms", "sausage", "onions"]);
+var p4 = pizzaOven("thin and crispy", "traditional", ["mozzarella", "chedder"], ["mushrooms", "sausage", "onions", "chicken"]);
 console.log(p4);
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
