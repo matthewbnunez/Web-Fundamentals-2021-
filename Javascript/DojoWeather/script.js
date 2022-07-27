b7ebfbab5914ba776c596423c4bfb8c7348c761c
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-07-26 21:17:26 -0700

    initial commit

---
+++ Javascript/DojoWeather/script.js
@@ -0,0 +1,23 @@
+function removeCookie() {
+    var cookie = document.querySelector(".cookies");
+    cookie.remove();
+}
+
+function changeTemp() {
+    var temp1 = document.querySelector("#temp1");
+    var temp2 = document.querySelector("#temp2");
+    var temp3 = document.querySelector("#temp3");
+    var temp4 = document.querySelector("#temp4");
+    var temp5 = document.querySelector("#temp5");
+    var temp6 = document.querySelector("#temp6");
+    var temp7 = document.querySelector("#temp7");
+    var temp8 = document.querySelector("#temp8");
+    temp1.innerHTML ="75°";
+    temp2.innerHTML ="65°";
+    temp3.innerHTML ="80°";
+    temp4.innerHTML ="66°";
+    temp5.innerHTML ="69°";
+    temp6.innerHTML ="61°";
+    temp7.innerHTML ="78°";
+    temp8.innerHTML ="70°";
+}
