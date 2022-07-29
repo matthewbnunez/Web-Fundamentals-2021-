04820f502075eb862cf5b454f80febc04eeed6a6
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-07-29 11:35:32 -0700

    initial commit

--- Javascript/4_Function_Calculator/script.js
+++ Javascript/4_Function_Calculator/script.js
@@ -1,11 +1,11 @@
-var Num = document.querySelector("#display");
+var displayDiv = document.querySelector("#display");
 var num1 = "";
 var num2 = "";
 var op = "";
 
 function press(element) {
     num1 += element;
-    Num.innerHTML = num1;
+    displayDiv.innerHTML = num1;
     console.log(num1);
 }
 
@@ -13,7 +13,7 @@ function setOP(element) {
     op = element;
     num2 = num1;
     num1 = "";
-    Num.innerHTML = num2;
+    displayDiv.innerHTML = num2;
     console.log(op);
 }
 
@@ -36,7 +36,7 @@ function calculate(element) {
     if (op == "/") {
         result = parseInt(num1) / parseInt(num2);
     }
-    Num.innerHTML = result;
+    displayDiv.innerHTML = result;
     console.log(result);
     num1 = "";
     num2 = "";
